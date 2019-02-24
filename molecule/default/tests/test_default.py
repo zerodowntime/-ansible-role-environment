c6b065a59237583a77dc680aee9650cef55c4c48
Krzysztof Kotewa <krzysztof.kotewa@zerodowntime.pl>
2019-02-24 21:32:07 +0100

    Bugfix Molecule: Replace incorect tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,8 +6,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_environment_file(host):
+    f = host.file('/etc/environment')
 
     assert f.exists
     assert f.user == 'root'
