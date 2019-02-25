49c8c3ae290d6c151e4b1a80471a98a54692c8f9
Krzysztof Kotewa <krzysztof.kotewa@zerodowntime.pl>
2019-02-25 08:33:56 +0100

    Fix: Replace invalid molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,6 @@ def test_environment_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+    assert f.mode == 0o0644
+    assert not f.contains('WHOAMI')
+    assert f.contains('EDITOR=vim')
